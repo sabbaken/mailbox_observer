2fd10a53cf219a966adeaa254f5ec3aa249690ab
Kirill Verenih <verenichkirill001@gmail.com>
2023-08-10 23:38:51 +0200

    add basic functional

--- base_station/base_station.ino
+++ base_station/base_station.ino
@@ -1,29 +1,79 @@
 #include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClientSecure.h>
 #include <UniversalTelegramBot.h>
 #include <LoRa.h>
 
-#define BOTtoken "your_bot_token"  // Your Bot's token obtained from BotFather
-#define WIFI_SSID "your_SSID"      // Your WiFi SSID
-#define WIFI_PASSWORD "your_PASSWORD"  // Your WiFi password
+#define BOTtoken "xxx"
+#define WIFI_SSID "xxx"
+#define WIFI_PASSWORD "xxx"
+#define CHAT_ID "xxx"
 
-WiFiClientSecure client;
-UniversalTelegramBot bot(BOTtoken, client);
+#define STATUS_CODE_MAILBOX_OPENED "o"
+#define STATUS_CODE_OBSERVER_RESTARTED "r"
+#define STATUS_CODE_ERROR_HALL_SENSOR_1 "eh1"
+
+const int csPin = D8;        // LoRa radio chip select
+const int resetPin = D3;     // LoRa radio reset
+const int irqPin = D2;       // LoRa hardware interrupt
+
+WiFiClientSecure wifiClient;
+UniversalTelegramBot bot(BOTtoken, wifiClient);
 
 void setup() {
-  LoRa.begin(915E6);  // Change this to your desired frequency.
+  Serial.begin(9600);
+  delay(10);
+
+  // Connect to Wi-Fi
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WIFI_SSID);
+  
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
+    Serial.print(".");
   }
+  
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+ 
+  // Ignore SSL/TLS certificate errors
+  wifiClient.setInsecure();
+
+  // Initialize LoRa with specific pins
+  LoRa.setPins(csPin, resetPin, irqPin);
+
+  // Start LoRa
+  if (!LoRa.begin(433E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+
+  LoRa.setCodingRate4(8);
+  LoRa.setSpreadingFactor(12);
 }
 
 void loop() {
+  // Try to parse packet
   int packetSize = LoRa.parsePacket();
-  if (packetSize) {
-    String receivedMessage = LoRa.readString();
-    if (receivedMessage == "Button pressed") {
-      String chat_id = "your_chat_id";
-      bot.sendMessage(chat_id, "Button pressed", "");
+  if(packetSize) {
+    String incoming = "";
+    while(LoRa.available()) {
+      incoming += (char)LoRa.read();
+    }
+    Serial.println(incoming);
+
+    if(incoming == STATUS_CODE_MAILBOX_OPENED) {
+      bot.sendMessage(CHAT_ID, "Mailbox was opened!", "");
+    }
+
+    if(incoming.length() > 0) {
+       bot.sendMessage(CHAT_ID, "Raw message: " + incoming, "");
     }
   }
 }
--- observer/observer.ino
+++ observer/observer.ino
@@ -1,17 +1,73 @@
+#include <ESP8266WiFi.h>
 #include <LoRa.h>
 
-#define BUTTON_PIN 2
+#define STATUS_CODE_MAILBOX_OPENED "o"
+#define STATUS_CODE_OBSERVER_RESTARTED "r"
+#define STATUS_CODE_ERROR_HALL_SENSOR_1 "eh1"
+
+const int csPin = D8;          // LoRa radio chip select
+const int resetPin = D3;       // LoRa radio reset
+const int irqPin = D2;         // LoRa hardware interrupt
+const int hallSensor1Pin = D0;  // Assuming D4 for Hall effect sensor input, adjust as needed
+const int ledPin = D4;         // Led pin
+
+bool errorHallSensor1 = false;
+bool mailboxOepenedFlag = false;
 
 void setup() {
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
-  LoRa.begin(915E6);  // Change this to your desired frequency.
+  Serial.begin(9600);
+  delay(10);
+
+  pinMode(hallSensor1Pin, INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);
+  
+  // Initialize LoRa with specific pins
+  LoRa.setPins(csPin, resetPin, irqPin);
+
+  // Start LoRa
+  if (!LoRa.begin(433E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+
+  LoRa.setCodingRate4(8);
+  LoRa.setSpreadingFactor(12);
+
+  transmitStatus(STATUS_CODE_OBSERVER_RESTARTED);
+
+  // delay(5000);
+
+  int hallSensor1 = digitalRead(hallSensor1Pin);
+
+  // Test if hall sensor can't detect magnet
+  if (hallSensor1 == HIGH) {
+    transmitStatus(STATUS_CODE_ERROR_HALL_SENSOR_1);
+    errorHallSensor1 = true;
+  }
 }
 
 void loop() {
-  if (digitalRead(BUTTON_PIN) == LOW) {
-    LoRa.beginPacket();
-    LoRa.print("Button pressed");
-    LoRa.endPacket();
-    delay(1000);  // A delay to debounce the button.
+  int hallSensor1 = digitalRead(hallSensor1Pin);
+  
+  if(!hallSensor1) {
+    mailboxOepenedFlag = false;
   }
+
+  if (!errorHallSensor1 && mailboxOepenedFlag == false && hallSensor1 == HIGH) {
+    digitalWrite(ledPin, LOW);
+    mailboxOepenedFlag = true;
+    Serial.println("Mail box is opened");
+    transmitStatus(STATUS_CODE_MAILBOX_OPENED);
+    delay(1000);
+    digitalWrite(ledPin, HIGH);
+  }
+
+  delay(10);
+}
+
+void transmitStatus(String status) {
+  LoRa.beginPacket();
+  LoRa.print(status);
+  LoRa.endPacket();
 }
